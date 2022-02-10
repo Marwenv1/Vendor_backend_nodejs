cce085170ac14db093a7c3de0981aea2fe9bd381
Marwenv1 <marwen.tlili@esprit.tn>
2022-02-10 13:29:26 +0100

    fix cors3

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+var corsOptions = {
+    origin: 'https://62049e21c5a12d3ec693afa6--nifty-hopper-99960d.netlify.app/',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+//app.use(cors());
 app.use(cookieParser());
 app.use(
   fileUpload({
