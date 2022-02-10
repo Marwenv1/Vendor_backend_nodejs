82dec53e8c70e39d238287b5c6192a89a05ec1d5
Marwenv1 <marwen.tlili@esprit.tn>
2022-02-10 15:38:34 +0100

    changed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 });*/
 //app.use(cors());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://6205204987c20f09033a0926--nifty-hopper-99960d.netlify.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
