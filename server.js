76911016655a6cab4cd31f98d2711659681ba5b6
Marwenv1 <marwen.tlili@esprit.tn>
2022-02-10 13:40:21 +0100

    fix cors4

--- server.js
+++ server.js
@@ -12,9 +12,26 @@ var allowCrossDomain = function(req, res, next) {
 
 
 const app = express();
-app.use(allowCrossDomain);
+//app.use(allowCrossDomain);
 app.use(express.json());
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://62049e21c5a12d3ec693afa6--nifty-hopper-99960d.netlify.app');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 //app.use(cors());
 app.use(cookieParser());
 app.use(
