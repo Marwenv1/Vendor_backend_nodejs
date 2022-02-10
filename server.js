9279cfe24ad552a658c9c0d6afd43fa36fc48cc1
Marwenv1 <marwen.tlili@esprit.tn>
2022-02-10 13:35:48 +0100

    fix cors4

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
-var corsOptions = {
-    origin: 'https://62049e21c5a12d3ec693afa6--nifty-hopper-99960d.netlify.app/',
-    optionsSuccessStatus: 200 // For legacy browser support
-}
+var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,      Accept");
+    next();
+};
 
 
 const app = express();
+app.use(allowCrossDomain);
 app.use(express.json());
-app.use(cors(corsOptions));
+
 //app.use(cors());
 app.use(cookieParser());
 app.use(
